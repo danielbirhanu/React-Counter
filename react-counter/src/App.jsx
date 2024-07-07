9673dcf90a324e0219fd75cfe61f862519aaaa3b
danielbirhanu <danielbir49@gmail.com>
2024-07-07 17:19:57 +0300

    Created and modified the Counter component

--- react-counter/src/App.jsx
+++ react-counter/src/App.jsx
@@ -1,11 +1,10 @@
-import React { useState } from 'react'
+import Counter from './Counter'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    
-  )
+    <Counter />
+  );
 }
 
 export default App
